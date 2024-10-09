53b032fb07c9bcef44b43196424ea430b71c7289
nzuwerag <gilbert.nzuwera@telpaygroup.com>
2024-10-09 16:53:45 +0200

    initial commit

---
+++ src/test/java/com/nzuwera/assignment/urlshortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nzuwera.assignment.urlshortner;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+    @Test
+    @Disabled
+    void contextLoads() {
+    }
+
+}
